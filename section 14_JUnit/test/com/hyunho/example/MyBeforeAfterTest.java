c90add00a6d43fc74908c2951f06ac7b6aa953fd
HYUHO LEE <leehh41563636@gmail.com>
2023-12-04 21:06:07 +0900

    JUnit 어노테이션 실험

---
+++ section 14_JUnit/test/com/hyunho/example/MyBeforeAfterTest.java
@@ -0,0 +1,36 @@
+package com.hyunho.example;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyBeforeAfterTest {
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("BeforeAll");
+    }
+    @BeforeEach
+    void beforeEach(){
+        System.out.println("BeforeEach");
+    }
+    @Test
+    void test1(){
+        System.out.println("test1");
+    }
+    @Test
+    void test2(){
+        System.out.println("test2");
+    }
+    @Test
+    void test3(){
+        System.out.println("test3");
+    }
+    @AfterEach
+    void AfterEach(){
+        System.out.println("AfterEach");
+    }
+    @AfterAll
+    static void AfterAll(){
+        System.out.println("AfterAll");
+    }
+}
